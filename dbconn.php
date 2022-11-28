b150ed4036ca654269e7661518336a36e1917e73
CyberFreak1911 <computerhackfusion@gmail.com>
2022-11-28 23:29:31 +0300

    Created database connection file to mysql server

---
+++ dbconn.php
@@ -0,0 +1,12 @@
+<?php
+    $dbHost = 'localhost';
+    $dbName = 'sams';
+    $dbUsername = 'yusa';
+    $dbPassword = '***REMOVED***';
+
+    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+    if (!$conn) {
+        die('Connection failed: '.mysqli_connect_error());
+      }
+
+?>
